73d32b944fe72a96938b56cbc27ab2a31db34f2f
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-08-03 01:39:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const animatedElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); 
+    }
+  });
+}, { threshold: 0.2 });
+
+animatedElements.forEach(el => observer.observe(el));
