564fbd773b9777ca45e61ccedb4de1c94d3cbca8
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-08-03 16:41:43 +0200

    animate project card added

--- scripts/animations.js
+++ scripts/animations.js
@@ -57,6 +57,47 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  const cards = document.querySelectorAll('.project-card');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        cards.forEach((card, index) => {
+          setTimeout(() => {
+            card.classList.add('show');
+          }, index * 1000); 
+        });
+        observer.disconnect();
+      }
+    });
+  }, { threshold: 0.2 });
+
+  observer.observe(document.querySelector('.projects'));
+
 
 });
+
+let cards = document.querySelectorAll('.project-card');
+
+cards.forEach(card =>{
+    card.addEventListener('mousemove', (e) => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;  
+
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+      const rotateX = ((y - centerY) / centerY) * 15;
+      const rotateY = ((x - centerX) / centerX) * 15;
+
+      card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+      card.style.transform = 'rotateX(0) rotateY(0) scale(1)';
+    });
+})
+
+
+
 
